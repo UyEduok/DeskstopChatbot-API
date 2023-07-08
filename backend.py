58a1e3fd5c6269d712872d75bcffc395757d81ab
UyEduok <uyeduok@gmail.com>
2023-07-08 17:54:37 +0100

    Created a chat bot app using PyQt6

---
+++ backend.py
@@ -0,0 +1,24 @@
+import openai
+import os
+
+
+class Chatbot:
+
+    def __init__(self):
+        openai.api_key = os.environ.get('chat_bot_key')
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+
+if __name__ == '__main__':
+    chatbot = Chatbot()
+    response = chatbot.get_response('what is love')
+    print(response)
+
